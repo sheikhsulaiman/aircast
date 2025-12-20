65e899d0b27936b745833217deab2c3b45e8cd14
Sheikh Sulaiman Sony <sheikhsulaimansony@outlook.com>
2025-12-20 19:56:28 +0600

    configure env

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,9 +4,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class WeatherService {
   final Dio _dio = Dio();
-
-  // Load API key from .env file
-  late final String _apiKey = dotenv.env['OPENWEATHER_API_KEY'] ?? '';
+  final String _apiKey = dotenv.env['OPENWEATHER_API_KEY'] ?? '';
   final String _baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
   WeatherService() {
